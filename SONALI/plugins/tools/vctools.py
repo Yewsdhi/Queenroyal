792c53ea3febdd05f0ed29ce8f7471f01d004e8a
BABY-MUSIC <tridevk442@gmail.com>
2024-08-24 11:51:34 +0530

    Update vctools.py

--- SONALI/plugins/tools/vctools.py
+++ SONALI/plugins/tools/vctools.py
@@ -19,13 +19,13 @@ from telethon.tl.functions.phone import (
 # vc on
 @app.on_message(filters.video_chat_started)
 async def brah(_, msg):
-    await msg.reply("**😍ᴠɪᴅᴇᴏ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ🥳**")
+    await msg.reply("**ᴠᴄ sᴛᴀʀᴛᴇᴅ ᴀʙ sᴏɴɢ ʙᴀᴊᴀᴏ**")
 
 
 # vc off
 @app.on_message(filters.video_chat_ended)
 async def brah2(_, msg):
-    await msg.reply("**😕ᴠɪᴅᴇᴏ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ💔**")
+    await msg.reply("**ᴠᴄ ᴇɴᴅ ᴏᴋ ʙʏᴇ**")
 
 
 # invite members on vc
