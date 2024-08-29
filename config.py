9337ff0235eab14b6af2c97c15cbf139b0407b9f
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-08-29 22:44:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -86,7 +86,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/1ade6b693439d63f99073.mp4"
+    "START_IMG_URL", "https://telegra.ph/file/1cbd56dca3d7e2a9a8477.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/bf626ed0d58fdae880574.jpg"
